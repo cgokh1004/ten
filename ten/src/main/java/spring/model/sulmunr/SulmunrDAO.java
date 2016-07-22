51d03afe1147d49495ffe979fc857a39b4311334
soldesk <soldesk@12-12>
2016-07-22 16:35:26 +0900

    커밋4

--- ten/src/main/java/spring/model/sulmunr/SulmunrDAO.java
+++ ten/src/main/java/spring/model/sulmunr/SulmunrDAO.java
@@ -11,6 +11,7 @@ public class SulmunrDAO implements DAOMyBatisInter {
 //테스트
 	//커밋1
 	//커밋2
+	//커밋4
 	@Override
 	public int create(Object dto, SqlSessionTemplate mybatis) throws Exception {
 		// TODO Auto-generated method stub
