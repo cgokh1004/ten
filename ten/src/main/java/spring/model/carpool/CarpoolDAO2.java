1506d4125fbfa6cde8104e086ab57b6d2757db06
soldesk <soldesk@3-4>
2016-07-22 12:24:04 +0900

    create

---
+++ ten/src/main/java/spring/model/carpool/CarpoolDAO2.java
@@ -0,0 +1,47 @@
+package spring.model.carpool;
+
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+
+import spring.model.ten.DAOMyBatisInter;
+
+public class CarpoolDAO2 implements DAOMyBatisInter {
+
+	@Override
+	public int create(Object dto, SqlSessionTemplate mybatis) throws Exception {
+			return mybatis.insert("carpool.create", dto);
+	}
+
+	@Override
+	public List list(Map map, SqlSessionTemplate mybatis) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object read(Object pk, SqlSessionTemplate mybatis) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int update(Object dto, SqlSessionTemplate mybatis) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Object pk, SqlSessionTemplate mybatis) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int total(Map map, SqlSessionTemplate mybatis) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
