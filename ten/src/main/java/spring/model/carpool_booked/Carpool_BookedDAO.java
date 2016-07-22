cc090bdda2dc5981a2f32b3ed91dd91c1de80898
soldesk <soldesk@3-4>
2016-07-22 14:55:38 +0900

    12311

--- ten/src/main/java/spring/model/carpool_booked/Carpool_BookedDAO.java
+++ ten/src/main/java/spring/model/carpool_booked/Carpool_BookedDAO.java
@@ -1,47 +1,44 @@
 package spring.model.carpool_booked;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
-@Component
-public class Carpool_BookedDAO {
-	@Autowired
-	private SqlSessionTemplate mybatis;
+import spring.model.ten.DAOMyBatisInter;
 
-	public void setMybatis(SqlSessionTemplate mybatis) {
-		this.mybatis = mybatis;
+public class Carpool_BookedDAO implements DAOMyBatisInter {
+
+	@Override
+	public int create(Object dto, SqlSessionTemplate mybatis) throws Exception {
+		return mybatis.insert("carpool_booked.create", dto);
 	}
 
-	public int create(Carpool_BookedDTO carpool_bookedDTO) {
-		return mybatis.insert("carpool_booked.create", carpool_bookedDTO);
-	}
-
-	public Carpool_BookedDTO read(int cp_bookedno) {
-		return mybatis.selectOne("carpool_booked.read", cp_bookedno);
-	}
-
-	public int update(Carpool_BookedDTO carpool_bookedDTO) {
-		return mybatis.update("carpool_booked.update", carpool_bookedDTO);
-	}
-
-	public int delete(int cp_bookedno) {
-		return mybatis.delete("carpool_booked.delete", cp_bookedno);
-	}
-
-	public List<Carpool_BookedDTO> list(Map map) {
+	@Override
+	public List list(Map map, SqlSessionTemplate mybatis) throws Exception {
 		return mybatis.selectList("carpool_booked.list", map);
 	}
 
-	public int total(String col, String word) {
-		Map map = new HashMap();
-		map.put("col", "");
-		map.put("word", "");
+	@Override
+	public Object read(Object pk, SqlSessionTemplate mybatis) throws Exception {
+		return mybatis.selectOne("carpool_booked.read", pk);
+
+	}
+
+	@Override
+	public int update(Object dto, SqlSessionTemplate mybatis) throws Exception {
+		return mybatis.update("carpool_booked.update", dto);
+
+	}
+
+	@Override
+	public int delete(Object pk, SqlSessionTemplate mybatis) throws Exception {
+		return mybatis.delete("carpool_booked.delete", pk);
+	}
+
+	@Override
+	public int total(Map map, SqlSessionTemplate mybatis) throws Exception {
 		return mybatis.selectOne("carpool_booked.total", map);
 	}
+
 }
